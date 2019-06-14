867225f3c9bd1d5a671c6a962a0c58f00f700041
MengZhang <meng.zhang15@ufl.edu>
2019-06-14 01:43:06 -0400

    change the default value for cropsite db api url

--- app/util/InterchangeConfig.scala
+++ app/util/InterchangeConfig.scala
@@ -5,7 +5,7 @@ import play.api.Play
 object InterchangeConfig {
   val myScheme = Play.current.configuration.getString("interchange.scheme").getOrElse("http")
   val tollboothUrl = rewriteUrl(Play.current.configuration.getString("tollbooth.baseurl").getOrElse("http://localhost:9110"))
-  val cropsitedbUrl = rewriteUrl(Play.current.configuration.getString("cropsitedb.baseurl").getOrElse("http://localhost:8080"))
+  val cropsitedbUrl = rewriteUrl(Play.current.configuration.getString("cropsitedb.baseurl").getOrElse("http://localhost:8080/cropsitedb/2"))
 
   def rewriteUrl(url: String):String = {
     if (url.endsWith("/")) {
